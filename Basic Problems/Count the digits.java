7e3b7e5ee53b18644c490645bf5a31ec02bd96af
VISHNU VARDHAN N.M <vishnuvardhann.m176@gmail.com>
2026-07-13 20:03:46 +0530

    Count the digits in the number

---
+++ Basic Problems/Count the digits.java
@@ -0,0 +1,16 @@
+/*
+Given a number n, return the count of digits in this number.
+*/
+
+class Solution {
+    public static int countDigits(int n) {
+        // Code here
+        int count=0;
+        while(n>0){
+            int num=n%10;
+            count=count + 1;
+            n=n/10;
+        }
+        return count;
+    }
+}
